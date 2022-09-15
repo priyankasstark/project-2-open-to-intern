afba31716b68cc42159653cb45ea46e23425e9f0
abcikram <roybikram59@gmail.com>
2022-09-15 13:21:11 +0530

    final project 2

--- src/controller/collegeController.js
+++ src/controller/collegeController.js
@@ -2,42 +2,85 @@ const collegeModel = require('../model/collegeModel')
 const internModel = require('../model/internModel')
 
 
+
+
+//_______________________________________________ Create College ______________________________________________________//
+
+
 const createCollege = async function(req, res){
     try{
+
         let collegeData = req.body
+        const nameRegex = /^[a-z]+$/
+        const fullnameRegex = /^[a-zA-Z\s]+$/
+        const logolinkRegex = /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/
+        
+        // validation for college name
+        if(!collegeData.name) return res.status(400).send({status : false, message : "College name is mandatory"})
 
-        if(!collegeData.name) return res.status(400).send({status : false, msg : "College name is mandatory"})
+        if (!collegeData.name.match(nameRegex)) return res.status(400).send({status:false, message: "name should be in lowercase"})  
+        
+        let college = await collegeModel.findOne({name: collegeData.name})
 
-        if(!collegeData.fullName) return res.status(400).send({status : false, msg : "Please provide full name of College"})
+        if(college) return res.status(400).send({status:false , message: "College already exist"})
 
-        if(!collegeData.logoLink) return res.status(400).send({status : false, msg : "Please provide Logolink"})
+        // validation for fullname 
+        if(!collegeData.fullName) return res.status(400).send({status : false, message : "Please provide full name of College"})
+      
+        if (!collegeData.fullName.match(fullnameRegex)) return res.status(400).send({status:false,msg: "name should be in lowercase"})  
+         
+        //validation for logoLink
+        if(!collegeData.logoLink) return res.status(400).send({status : false, message : "Please provide Logolink"})
 
+        if (!collegeData.logoLink.match(logolinkRegex)) return res.status(400).send({ status:false , message: "logolink is invalid"})  
+
+        // College is created
         let createCollege = await collegeModel.create(collegeData)
-        return res.status(201).send({ status : true, msg :"college created successfully", data : createCollege})
+        // using destructure find the key value of College
+        const { name,fullName,logoLink } = createCollege
+        
+        // response hit at postman
+        let obj = {
+            name : name,
+            fullName : fullName,
+            logoLink : logoLink,
+            isDeleted : false
+        }
+        
+        return res.status(201).send({ status : true , data : obj})
+
     }
     catch (err) {
-        return res.status(500).send({ status : false, msg : err.message})
+        return res.status(500).send({ status : false, message : err.message})
     }
 }
 
 
-//_____________________________________Get college Interns______________________________________________-
+//_____________________________________________ Get college Interns ______________________________________________________//
+
 
 const getList = async function(req,res){
     try{
         let data = req.query
-        if(Object.keys(data) == 0) return res.status(400).send({status : false, msg : "Please provide College Name"})
         
+        if(Object.keys(data) == 0) return res.status(400).send({status : false, message : "Please provide College Name"})
+        
+        // finding the valid college 
         let findCollege = await collegeModel.findOne({name : data.name, isDeleted : false})
+        
+        if(!findCollege) return res.status(404).send({status:false , message:"No such college"})
+
         const {name, fullName, logoLink} = findCollege
     
-        if(findCollege.length == 0) return res.status(404).send({status : false, msg : "No such a College"})
+        if(findCollege.length == 0) return res.status(404).send({status : false, message : "No such a College"})
         //console.log(findCollege);
 
         let CollegeId = findCollege._id
         //console.log(CollegeId)
 
         let findIntern = await internModel.find({collegeId : CollegeId}).select({name : 1, email : 1, mobile : 1})
+        
+        if(findIntern.length == 0) return res.send({status:false , message:"No intern is apply"})
         
         let obj = {
             name : name,
@@ -46,7 +89,7 @@ const getList = async function(req,res){
             interns : findIntern
         }
 
-        return res.status(400).send({data : obj})
+        return res.status(200).send({status: true ,data : obj})
     }
     catch(err){
         res.status(500).send({status : false, error : err.message})
@@ -54,6 +97,8 @@ const getList = async function(req,res){
 }
 
 
+//_______________________________________ module export _____________________________________________//
 
 module.exports.createCollege= createCollege;
+
 module.exports.getList = getList
--- src/controller/internController.js
+++ src/controller/internController.js
@@ -1,26 +1,40 @@
 const internModel = require("../model/internModel")
 const collegeModel = require("../model/collegeModel")
 
+
+
+//____________________________________________ Create Intern __________________________________________________________//
+
+
 const createIntern = async function(req,res){
     try{
         let data = req.body
         let collegeName = data.collegeName
+        
         const emailRegex = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/
-        const nameRegex = /^[a-z\s]+$/i
+        const nameRegex = /^[a-zA-Z\s]+$/
         const mobileRegex = /^[0-9]{10}$/
 
         if(Object.keys(data).length === 0) return res.status(400).send({status : false, msg : "Provide data. Body can't be empty!"})
-
+        
+        // validation for intern name
         if(!data.name) return res.status(400).send({status : false, msg : "Name is Mandatory"})
 
         if(!data.name.match(nameRegex)) return res.status(400).send({status : false, msg : "Invalid format of Name"})
-
+       
+        // validation for college name 
         if(!collegeName) return res.status(400).send({status : false, msg : "College Name is mandatory"})
-
+        
+        //validation for mobile
         if (!data.mobile) return res.status(400).send({ Status: false, msg: "mobile is required" })
 
         if(!data.mobile.match(mobileRegex)) return res.status(400).send({status : false, msg : "Invalid format of mobile number"})
         
+        let validmobile = await internModel.findOne({mobile : data.mobile})
+
+        if (validmobile)  return res.status(400).send({status: false ,msg:"Mobilenumber is already exist"})
+        
+        // validation for email
         if (!data.email) return res.status(400).send({ Status: false, msg: "email is required" })
 
         if(!data.email.match(emailRegex)) return res.status(400).send({status : false, msg : "Invalid Format of Email"})
@@ -28,17 +42,19 @@ const createIntern = async function(req,res){
         let validEmail = await internModel.findOne({email : data.email})
 
         if(validEmail) return res.status(400).send({status : false, msg : "Email already exist"})
-               
+
         let findCollege = await collegeModel.findOne({name : collegeName})
+         // if college doesnot exist 
         if(!findCollege){
             return res.status(404).send({status : false, msg : "No such a College"})
         }
-
-        let CollegeId = findCollege._id.toString()
-        data.collegeId = CollegeId // added college Id in DB 
-        let intern = await internModel.create(data)
-        //console.log(intern)
         
+        let CollegeId = findCollege._id.toString()
+        data.collegeId = CollegeId // added college Id in DB // collegeId is the key which we insert for our requirement
+        //CollegeId is the id of findCollege
+        let intern = await internModel.create(data)
+        
+        // it is only for response at postman
         let internresponse = {
             name : data.name,
             email : data.email,
@@ -47,7 +63,7 @@ const createIntern = async function(req,res){
             isDeleted : false
         }
 
-        return res.status(200).send({status : true, msg : "Intern is created", data : internresponse})
+        return res.status(200).send({status : true , data : internresponse})
 
     }
 
@@ -55,5 +71,7 @@ const createIntern = async function(req,res){
         res.status(500).send({status : false, error : err.message})
     }
 }
+
+//_____________________________________ module export ____________________________________________________________// 
 
 module.exports.createIntern = createIntern
